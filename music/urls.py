675fa3dc8693c23866704a77d0a659c1105ca150
Siddharth <sidjain1412@gmail.com>
2018-10-12 03:29:30 +0530

    Small changes to admin file, added url for /music/album_id

--- music/urls.py
+++ music/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    # /music/
+    path('', views.index, name='index'),
+
+    # /music/23
+    path('<int:album_id>', views.detail, name='detail')
 ]
