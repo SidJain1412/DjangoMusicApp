f81c6888b8d749d32865ff64b953b8736b7e819e
Siddharth <sidjain1412@gmail.com>
2018-10-12 21:37:10 +0530

    Added working templates with titles, hrefs, etc. 404 added, small changes

--- music/views.py
+++ music/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Album
-from django.template import loader
+from django.http import Http404
+# from django.template import loader
 # Create your views here.
 
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get.template('music/index.html')
-    context = {'all_albums': all_albums}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album ID: " + str(album_id) + "</h2>")
+    try:
+        album_id = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("That album does not exist")
+    return render(request, 'music/detail.html', {'album_id': album_id})
