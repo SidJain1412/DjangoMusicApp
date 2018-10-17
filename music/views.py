ef03161513a879e15213eca951f6bc6221c8288e
Siddharth <sidjain1412@gmail.com>
2018-10-17 23:14:02 +0530

    Added CSS, used bootstrap to make a basic navbar, added a background, added a couple links here and there

--- music/views.py
+++ music/views.py
@@ -18,13 +18,23 @@ def detail(request, album_id):
 def favorite(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
     try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
+        # Using getlist because checkbox gives a list
+        ticked_songs = request.POST.getlist('song')
+        print(ticked_songs)
+        for i in ticked_songs:
+            selected_song = album.song_set.get(pk=i)
+            print(selected_song.is_favorite)
+            if selected_song.is_favorite:
+                selected_song.is_favorite = False
+            else:
+                selected_song.is_favorite = True
+            selected_song.save()
     except (KeyError, Song.DoesNotExist):
         return render(request, 'music/detail.html', {
             'album': album,
             'error_message': "Select a valid song please"
         })
-    else:
-        selected_song.is_favorite = True
-        selected_song.save()
-        return render(request, 'music/detail.html', {'album': album})
+    # else:
+    #     selected_song.is_favorite = True
+    #     selected_song.save()
+    return render(request, 'music/detail.html', {'album': album})
